e4f999d3251d65b40da62e3f6831ad97729c031a
Ravi Chitroda <ravi.chitroda91@gmail.com>
2022-09-15 11:41:28 +0530

    Initial commit

---
+++ src/Tour.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Tour = () => {
+  return (
+    <div>
+      <h3>Tour container</h3>
+    </div>
+  );
+};
---
+++ src/Tours.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tours = () => {
+  return (
+    <div>
+      <h1>Tour Page</h1>
+    </div>
+  );
+};
+
+export default Tours;

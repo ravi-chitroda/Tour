57c2a4b5095de21b785cb58168b9f5ae1ab494b2
Ravi Chitroda <ravi.chitroda91@gmail.com>
2022-09-15 15:47:28 +0530

    Toggel of info section with "Show More" and "Show Less" we can read less and more with this.

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Tour = ({ id, image, info, price, name }) => {
+  const [showMore, setShowMore] = useState(false);
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -9,7 +11,12 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price"> &#8377; {price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {showMore ? info : `${info.substring(0, 150)}...`}
+          <button onClick={() => setShowMore(!showMore)}>
+            {showMore ? "Show Less" : "Show More"}
+          </button>
+        </p>
         <button className="delete-btn">not Interested</button>
       </footer>
     </article>
